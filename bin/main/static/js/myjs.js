5199cb02b65234ffee65c8c31a9de5dc7a875dd3
wodlr7777 <59951799+wodlr7777@users.noreply.github.com>
2020-01-29 16:39:45 +0900

    top.html js설정
    a태그 js설정

--- bin/main/static/js/myjs.js
+++ bin/main/static/js/myjs.js
@@ -4,6 +4,7 @@
 function cate(cate) {
 	$(".categori li").css("background-color","white");
 	$(".categori a").css("color","black");
+	$(".user-categori a").css("color","gray").css("font-weight","500");
 	if(cate=='left'){
 		$(".cate-left").css("background-color","black");
 		$(".cate-left a").css("color","#fff");
@@ -13,5 +14,19 @@ function cate(cate) {
 	}else if(cate=='right'){
 		$(".cate-right").css("background-color","black");
 		$(".cate-right a").css("color","#fff");
+	}
+}
+
+function uc(uc) {
+	$(".user-categori a").css("color","gray").css("font-weight","500");
+	if(uc=='new'){
+		$(".uc1 a").css("color","black").css("font-weight","bold");
+	}else if(uc=='popular'){
+		$(".uc2 a").css("color","black").css("font-weight","bold");
+	}else if(uc=='low'){
+		$(".uc3 a").css("color","black").css("font-weight","bold");
+	}
+	else if(uc=='high'){
+		$(".uc4 a").css("color","black").css("font-weight","bold");
 	}
 }
