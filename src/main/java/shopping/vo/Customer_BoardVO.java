877fd6ba9ac1512af1bd3cf28391fa94935bb90d
w-beom <60081555+w-beom@users.noreply.github.com>
2020-03-27 18:11:43 +0900

    lombok 미사용으로 인한 코드수정
    lombok 미사용으로 인한 코드수정

--- src/main/java/shopping/vo/Customer_BoardVO.java
+++ src/main/java/shopping/vo/Customer_BoardVO.java
@@ -3,11 +3,7 @@ package shopping.vo;
 
 import java.sql.Timestamp;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
 public class Customer_BoardVO {
 	private int customer_id;
 	private String customer_subject;
@@ -15,6 +11,47 @@ public class Customer_BoardVO {
 	private String id;
 	private int user_id;
 	private Timestamp customer_date;
+	
+	public Customer_BoardVO() {}
+	
+	public int getCustomer_id() {
+		return customer_id;
+	}
+	public void setCustomer_id(int customer_id) {
+		this.customer_id = customer_id;
+	}
+	public String getCustomer_subject() {
+		return customer_subject;
+	}
+	public void setCustomer_subject(String customer_subject) {
+		this.customer_subject = customer_subject;
+	}
+	public String getCustomer_content() {
+		return customer_content;
+	}
+	public void setCustomer_content(String customer_content) {
+		this.customer_content = customer_content;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public Timestamp getCustomer_date() {
+		return customer_date;
+	}
+	public void setCustomer_date(Timestamp customer_date) {
+		this.customer_date = customer_date;
+	}
+	
+	
 	
 	
 }
--- src/main/java/shopping/vo/UserVO.java
+++ src/main/java/shopping/vo/UserVO.java
@@ -5,7 +5,6 @@ import java.time.format.DateTimeFormatter;
 
 import lombok.Data;
 
-@Data
 public class UserVO {
 	private int user_id;
 	private String id;
@@ -18,15 +17,101 @@ public class UserVO {
 	private int month;
 	private int day;
 	private String birth;
-	
-	public UserVO() {
-		
+
+	public UserVO() {}
+
+	public int getUser_id() {
+		return user_id;
 	}
-	
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public void setMonth(int month) {
+		this.month = month;
+	}
+
+	public int getDay() {
+		return day;
+	}
+
+	public void setDay(int day) {
+		this.day = day;
+	}
+
+	public String getBirth() {
+		return birth;
+	}
+
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+
 	public void insertBirth() {
 		String date = LocalDate.of(this.year, this.month, this.day).format(DateTimeFormatter.BASIC_ISO_DATE);
 		birth = date;
-		
+
 	}
 
 }
